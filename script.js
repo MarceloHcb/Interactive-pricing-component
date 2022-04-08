9dce8c90cad8695e6ec17127142efd6824d96a4d
MarceloHcb <marcelohcb@msn.com>
2022-04-08 18:46:28 -0300

    upgrade

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const value = document.querySelector("#value")
 const range = document.querySelector("input")
-const circle = document.querySelector(".circle")
 const spann = document.getElementById("span")
 const progressBar = document.getElementById("progressBar")
-console.log(spann)
-console.log(circle)
+const bgCircle = document.querySelector(".bg-circle")
+
 progressBar.style.width = "34px"
 
 range.oninput = (()=>{
-    const rangeValue = range.value
+    const rangeValue = range.value     
     value.textContent = `$${rangeValue},00` 
-    progressBar.style.width = rangeValue*2.1 + "px"
+    progressBar.style.width = rangeValue*2.1 + "px"      
+
 })
 spann.addEventListener("click",()=>{    
-     spann.classList.toggle("bgClick")     
+     spann.classList.toggle("bgClick") 
+     bgCircle.classList.toggle("active") 
 })
 
     
